d06affadd511613262b8736369294a61465b0ac5
Henrique <henriquel1997@hotmail.com>
2019-06-04 11:47:30 -0300

    Initial commit

---
+++ util.h
@@ -0,0 +1,21 @@
+//
+// Created by Henrique on 04/06/2019.
+//
+
+#ifndef RAYTRACERCHALLENGE_UTIL_H
+#define RAYTRACERCHALLENGE_UTIL_H
+
+#define EPSILON 0.00001
+
+float abs(float f){
+    if(f < 0){
+        f = f * -1;
+    }
+    return f;
+}
+
+bool equal(float f1, float f2){
+    return abs(f1 - f2) < EPSILON;
+}
+
+#endif //RAYTRACERCHALLENGE_UTIL_H
